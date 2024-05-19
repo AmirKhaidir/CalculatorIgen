0159cd2356d1ee04578db937bd4b4b0a1b48ab30
AmirKhaidir <a.khaidir98@gmail.com>
2024-05-19 23:04:48 +0800

    Fixed Multiply and Division Bodmas Checking

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,81 +4,57 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
-    // public static void main(String[] args) throws Exception {
-        // String test1 = "1 + 1";
-        // String test2 = "2 * 2";
-        // String test3 = "1 + 2 + 3";
-        // String test4 = "6 / 2";
-        // String test5 = "11 + 23";
-        // String test6 = "11.1 + 23";
-        // String test7 = "1 + 1 * 3";
-        // String test8 = "( 11.5 + 15.4 ) + 10.1";
-        // String test9 = "23 - ( 29.3 - 12.5 )";
-        // String test10 = "10 - ( 2 + 3 * ( 7 - 5 ) )";
-        // String test11 = "2 * 3 / 4 + 8 - 1";
-        // String test12 = "4 + 8 - 1 * 2 / 2";
-        // String test13 = "2 * 2 * 2";
-        // System.out.println("test1 " + test1 + ": " + calculate(test1) + "\n");
-        // System.out.println("test2 " + test2 + ": " + calculate(test2) + "\n");
-        // System.out.println("test3 " + test3 + ": " + calculate(test3) + "\n");
-        // System.out.println("test4 " + test4 + ": " + calculate(test4) + "\n");
-        // System.out.println("test5 " + test5 + ": " + calculate(test5) + "\n");
-        // System.out.println("test6 " + test6 + ": " + calculate(test6) + "\n");
-        // System.out.println("test7 " + test7 + ": " + calculate(test7) + "\n");
-        // System.out.println("test8 " + test8 + ": " + calculate(test8) + "\n");
-        // System.out.println("test9 " + test9 + ": " + calculate(test9) + "\n");
-        // System.out.println("test10 " + test10 + ": " + calculate(test10) + "\n");
-        // System.out.println("test11 " + test11 + ": " + calculate(test11) + "\n");
-        // System.out.println("test12 " + test12 + ": " + calculate(test12) + "\n");
-        // System.out.println("test13 " + test13 + ": " + calculate(test13) + "\n");
-    // }
 
     public static double calculate(String sum) {
         System.out.println("Calculate: " + sum + "\n");
         String[] arrayDigit = sum.split(" ");
         List<String> digits = new ArrayList(Arrays.asList(arrayDigit));
         String operatorReg = "+-*/()";
-        String pmReg = "+-";
+        String mulDiv = "*/";
         double total = 0;
 
         boolean isCompleted = false;
         boolean isBodMas = true;
         boolean isEnd = false;
+
         while (!isCompleted) {
             boolean isBracket = false;
+
             for (int i = 0; i < digits.size(); i++) {
                 if (operatorReg.contains(digits.get(i))) {
                     boolean isCalculated = false;
+                    
+                    // Check for bracket first, prioritize parenthesis according to BODMAS
                     if (digits.get(i).equals("(")) {
                         List<String> subSum = new ArrayList<String>();
                         isBracket = true;
                         int bracketLength = digits.lastIndexOf(")");
+
+                        // Create a sublist for equation in parenthesis
                         for (int j = i + 1; j < bracketLength; j++) {
                             subSum.add(digits.get(j));
                         }
-                        System.out.println(String.join("subSum: " + subSum));
+
                         total = calculate(String.join(" ", subSum));
                         isCalculated = true;
-
+                        
+                        // Set the parenthesis operation total
                         digits.set(bracketLength, String.valueOf(total));
                         digits.subList(i, bracketLength).clear();
-                        System.out.println(digits);
                     }
                     else {
                         if (!isBracket) {
-                            if (digits.get(i).equals("/")) {
-                                total = arimethericOperation(Double.parseDouble(digits.get(i - 1)), Double.parseDouble(digits.get(i + 1)), digits.get(i));
-                                isCalculated = true;
-                                isBodMas = true;
-                            }
-                            else if (digits.get(i).equals("*")) {
+                            // Check for multiply or Division
+                            if (mulDiv.contains(digits.get(i))) {
+                                // To maker sure there is no bracker after first scanning
                                 if (isEnd) {
                                     total = arimethericOperation(Double.parseDouble(digits.get(i - 1)), Double.parseDouble(digits.get(i + 1)), digits.get(i));
                                     isCalculated = true;
                                     isBodMas = true;
-                                } 
+                                }
                             }
                             else {
+                                // Only do add or substract if already done the first scanning and no multiply or division found
                                 if (!isBodMas && isEnd) {
                                     total = arimethericOperation(Double.parseDouble(digits.get(i - 1)), Double.parseDouble(digits.get(i + 1)), digits.get(i));
                                     isCalculated = true;
@@ -86,7 +62,8 @@ public class Calculator {
                             }
                         }
                     }
-
+                    
+                    // If any calculation done, replace the first digit, operator and second digit to the result of operation
                     if (isCalculated) {
                         if (!isBracket) {
                             digits.set(i, String.valueOf(total));
@@ -96,16 +73,20 @@ public class Calculator {
                     }
                 }
             }
+
+            // Reset the bodmas after first scanning
             if (isEnd) {
                 isBodMas = false;
             }
+
             isEnd = true;
-            // System.out.println("digits.size: " + digits.size());
+
+            // If only 1 element in list (it is the result), end the while loop
             if (digits.size() == 1) {
                 isCompleted = true;
             }
-        }         
- 
+        }
+
         return total;
     }
 
